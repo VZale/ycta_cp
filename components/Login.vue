c32b583515c69b1ddb1d5bec8f275220488e2eb5
VladZale <zaliotin2012@yandex.ru>
2023-03-26 14:46:11 +0400

    INIT

---
+++ components/Login.vue
@@ -0,0 +1,204 @@
+<template>
+  <div class="login">
+    <div class="container">
+      <div class="left">
+        <div class="box">
+          <a class="logo" href="">
+            <img :src="require('@/assets/ycta-logo-white.png')" alt="">
+          </a>
+          <div class="content">
+            <h1>Добро пожаловать!</h1>
+            <p>Чтобы начать работу в панели администратора, необходимо авторизоваться</p>
+          </div>
+        </div>
+      </div>
+      <div class="right">
+        <div class="box">
+          <h2>Авторизация</h2>
+          <div class="item form">
+            <div class="form-item" :class="{'is-focus': inputIsActive['login']}">
+              <p>Ваше имя</p>
+              <input v-model="userInfo.login" type="text" @focus="setFocus('login')" @blur="removeFocus()">
+            </div>
+            <div class="form-item" :class="{'is-focus': inputIsActive['password']}">
+              <p>Номер телефона</p>
+              <input v-model="userInfo.password" type="text" @focus="setFocus('password')"
+                     @blur="removeFocus()">
+            </div>
+            <div class="autologin">
+              <button class="button red large" @click="send()">Войти</button>
+              <p>
+                Запомнить меня на этом устройстве
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Vue from "vue"
+
+export default {
+  name: "Login",
+  data() {
+    return {
+      inputIsActive: {
+        login: false,
+        password: false
+      },
+      userInfo: {
+        login: '',
+        password: ''
+      },
+    }
+  },
+  methods: {
+    setFocus(item) {
+      this.$set(this.inputIsActive, item, true)
+    },
+    removeFocus() {
+      for (const item in this.inputIsActive) {
+        if (!this.userInfo[item]) {
+          this.$set(this.inputIsActive, item, false)
+        }
+      }
+    },
+    send() {
+      Vue.set(this, 'applicationModal', true)
+    },
+  }
+}
+</script>
+
+<style scoped>
+.login .container {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 12px;
+  padding: 40px;
+  height: 100vh;
+}
+
+.login h2 {
+  font-weight: 500;
+  font-size: 40px;
+  line-height: 110%;
+  text-align: center;
+  width: 100%;
+  margin-bottom: 40px;
+}
+
+.right,
+.left {
+  display: grid;
+  align-items: center;
+  border-radius: 12px;
+}
+
+.right {
+  background-color: var(--white);
+  padding: 0 140px;
+}
+
+.left {
+  position: relative;
+  color: var(--white);
+  padding: 0 40px;
+  overflow: hidden;
+}
+
+.left::after {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-repeat: no-repeat;
+  z-index: -1;
+  background-size: cover;
+  background-image: url("./assets/login-bg.png");
+}
+
+.left .box {
+  display: grid;
+  grid-template-rows: repeat(2, max-content);
+  height: 100%;
+  place-content: space-between;
+  padding: 40px 0 60px;
+}
+
+h1 {
+  font-weight: 500;
+  font-size: 72px;
+  line-height: 105%;
+  margin-bottom: 24px;
+}
+
+.left p {
+  font-weight: 400;
+  font-size: 18px;
+  line-height: 140%;
+}
+
+.form-item {
+  background: var(--gray-1);
+  border-radius: 8px;
+  height: 53px;
+  position: relative;
+  margin-bottom: 12px;
+}
+
+.form-item:last-child {
+  margin-bottom: 0;
+}
+
+.form-item:focus {
+  background-color: black;
+}
+
+.form-item p {
+  position: absolute;
+  top: 50%;
+  left: 14px;
+  transform: translateY(-50%);
+  color: var(--gray-4);
+  z-index: 2;
+  transition: all .3s ease-in-out;
+}
+
+.form-item.is-focus p {
+  font-size: 12px;
+  top: 17px;
+}
+
+input {
+  height: 100%;
+  width: 100%;
+  outline: none;
+  background-color: transparent;
+  color: var(--white);
+  font-size: 18px;
+  position: relative;
+  z-index: 3;
+  font-weight: 400;
+}
+
+.autologin button {
+  margin: 24px 0;
+}
+
+.autologin p {
+  padding-left: 25px;
+}
+
+button {
+  font-size: 18px;
+  width: 100%;
+  text-align: center;
+  font-weight: 500;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,66 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'ycta_cp',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      {charset: 'utf-8'},
+      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      {hid: 'description', name: 'description', content: ''},
+      {name: 'format-detection', content: 'telephone=no'}
+    ],
+    link: [
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'},
+      {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
+      {rel: 'preconnect', href: 'https://fonts.gstatic.com'},
+      {rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500&display=swap'}
+    ]
+  },
+  server: {
+    host: process.env.NUXT_HOST,
+    port: process.env.NUXT_PORT,
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+    'css/general'
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [],
+
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.push({
+        name: 'Главная',
+        path: '/',
+        component: resolve(__dirname, 'pages/index.vue'),
+        title: 'Главная'
+      })
+    }
+  },
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {}
+}
