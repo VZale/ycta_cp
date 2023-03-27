2da145caeadad65c61c5cec581e7accc680a18c9
VladZale <zaliotin2012@yandex.ru>
2023-03-27 20:17:12 +0400

    Added favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       {name: 'format-detection', content: 'telephone=no'}
     ],
     link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.png'},
       {rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons'},
       {rel: 'preconnect', href: 'https://fonts.googleapis.com'},
       {rel: 'preconnect', href: 'https://fonts.gstatic.com'},
